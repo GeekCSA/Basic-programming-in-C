74b8475b26936f1c1bd92cf04b6929b9cd8033bf
GeekCSA <33448208+GeekCSA@users.noreply.github.com>
2017-11-19 12:04:51 +0200

    Gnim

---
+++ Player.h
@@ -0,0 +1,13 @@
+/*
+ * Player.h
+ *
+ *  Created on: Nov 12, 2017
+ *      Author: moshe
+ */
+
+#ifndef PLAYER_H_
+#define PLAYER_H_
+
+void play(int left,int center, int right, int turn,int bigPacket, int mediumPacket, int smallPacket);
+
+#endif /* PLAYER_H_ */
